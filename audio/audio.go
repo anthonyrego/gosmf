4224a1a65dcba4ea44fc8f3ca8086f810db585bc
Anthony Rego <rego.zfactory@gmail.com>
2016-06-11 22:37:26 -0400

    Split out to be OS specific

--- audio/audio.go
+++ audio/audio.go
@@ -1,45 +1,9 @@
 package audio
 
-/*
-#cgo LDFLAGS: -framework OpenAL
-#include <CoreFoundation/CoreFoundation.h>
-#include <AudioToolbox/AudioToolbox.h>
-#import <OpenAL/al.h>
-#import <OpenAL/alc.h>
-
-ALCcontext *context;
-ALCdevice *device;
-
-void openAudioDevice() {
-  const ALCchar *default_device;
-
-  default_device = alcGetString(NULL, ALC_DEFAULT_DEVICE_SPECIFIER);
-  if ((device = alcOpenDevice(default_device)) == NULL)
-  {
-    fprintf(stderr, "failed to open sound device\n");
-    return;
-  }
-  context = alcCreateContext(device, NULL);
-
-  alcMakeContextCurrent(context);
-
-  alcProcessContext(context);
-  alGetError();
-}
-
-void closeAudioDevice()
-{
-  alcMakeContextCurrent(NULL);
-  alcDestroyContext(context);
-  alcCloseDevice(device);
-}
-*/
-import "C"
-
 func Init() {
-	C.openAudioDevice()
+	InitDevice()
 }
 
 func Cleanup() {
-	C.closeAudioDevice()
+	DestroyDevice()
 }
