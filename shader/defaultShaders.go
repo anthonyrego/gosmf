8a1b2e8fcc45ae0d9fcbdc530f851998d65ab50e
Anthony Rego <anthony.rego@omakasecorp.com>
2017-03-09 16:38:03 -0500

    Add color shader and start of simple shape package

--- shader/defaultShaders.go
+++ shader/defaultShaders.go
@@ -32,4 +32,33 @@ var shaderList = map[string]Shader{
 		id:     0,
 		loaded: false,
 	},
+	"color": Shader{
+		vert: `
+		#version 330
+
+		uniform mat4 projection;
+		uniform mat4 camera;
+		uniform mat4 model;
+
+		in vec3 vert;
+    in vec4 color;
+    out vec4 colorV;
+
+		void main() {
+				colorV = color;
+				gl_Position = projection * camera *  model * vec4(vert, 1);
+		}
+		` + "\x00",
+		frag: `
+		#version 330
+		in vec4 colorV;
+		out vec4 finalColor;
+
+		void main() {
+				finalColor = colorV;
+		}
+		` + "\x00",
+		id:     0,
+		loaded: false,
+	},
 }
---
+++ shape/block.go
@@ -0,0 +1,78 @@
+package shape
+
+import (
+	"log"
+
+	"github.com/anthonyrego/gosmf/shader"
+	"github.com/go-gl/gl/v4.1-core/gl"
+	"github.com/go-gl/mathgl/mgl32"
+)
+
+// Block is the struct for displaying a colored block
+type Block struct {
+	vao uint32
+}
+
+// New creates and returns a new Block object
+func NewBlock(width, height, r, g, b, a int) (*Block, error) {
+	block := &Block{}
+	err := block.create(width, height, float32(r)/255.0, float32(g)/255.0, float32(b)/255.0, float32(a)/255.0)
+	if err != nil {
+		log.Fatalln("failed to create block shape:", err)
+		return nil, err
+	}
+	return block, nil
+}
+
+func (block *Block) create(width, height int, r, g, b, a float32) error {
+	var vao uint32
+
+	gl.GenVertexArrays(1, &vao)
+	gl.BindVertexArray(vao)
+
+	var vbo uint32
+	gl.GenBuffers(1, &vbo)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+
+	w := float32(width)
+	h := float32(height)
+
+	shapeVertices := []float32{
+		w, h, 0.0, r, g, b, a,
+		0.0, 0.0, 0.0, r, g, b, a,
+		0.0, h, 0.0, r, g, b, a,
+
+		w, h, 0.0, r, g, b, a,
+		0.0, 0.0, 0.0, r, g, b, a,
+		w, 0.0, 0.0, r, g, b, a,
+	}
+
+	gl.BufferData(gl.ARRAY_BUFFER, len(shapeVertices)*4, gl.Ptr(shapeVertices), gl.STATIC_DRAW)
+
+	vertAttrib := uint32(0)
+	gl.EnableVertexAttribArray(vertAttrib)
+	gl.VertexAttribPointer(vertAttrib, 3, gl.FLOAT, false, 7*4, gl.PtrOffset(0))
+
+	colorCoordAttrib := uint32(1)
+	gl.EnableVertexAttribArray(colorCoordAttrib)
+	gl.VertexAttribPointer(colorCoordAttrib, 4, gl.FLOAT, false, 7*4, gl.PtrOffset(3*4))
+
+	block.vao = vao
+
+	return nil
+}
+
+// Draw will draw the block in the x,y and z
+func (block *Block) Draw(x float32, y float32, z float32, scale float32) {
+
+	model := mgl32.Translate3D(x, y, z)
+	model = model.Mul4(mgl32.Scale3D(scale, scale, 1))
+	// remember this is in radians!
+	// model = model.Mul4(mgl32.HomogRotate3D(mgl32.DegToRad(90), mgl32.Vec3{0, 0, 1}))
+	if shader := shader.GetActive(); shader != nil {
+		gl.UniformMatrix4fv(shader.Model, 1, false, &model[0])
+	}
+
+	gl.BindVertexArray(block.vao)
+	gl.DrawArrays(gl.TRIANGLES, 0, 6)
+}
