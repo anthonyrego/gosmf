f81db2b578362cd241544cc1a0ec5cf942adf222
Anthony Rego <rego.zfactory@gmail.com>
2018-05-06 02:41:38 -0400

    Change linking of sdl library

--- window/build.go
+++ window/build.go
@@ -2,7 +2,7 @@ package window
 
 /*
 #cgo darwin CFLAGS:   -D_GOSMF_OSX_
-#cgo darwin LDFLAGS:  -F/Library/Frameworks -framework SDL2
+#cgo darwin LDFLAGS:  -L/usr/local/lib -lSDL2
 
 #cgo linux CFLAGS:    -D_GOSMF_LINUX_
 #cgo linux LDFLAGS:   -lSDL2main -lSDL2
